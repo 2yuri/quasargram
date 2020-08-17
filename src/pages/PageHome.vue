0940ff6d3c2bf7cdd7c65972f220a52e068356da
Yuri Alves Miguel <yuri.snp@hotmail.com>
2020-08-16 21:54:17 -0300

    finish home-page module

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,53 +1,68 @@
 <template>
-  <q-page class="constrain q-pa-md">
-    <q-card
-      v-for="post in posts"
-      :key="post.id"
-      class="card-post q-mb-md"
-      flat
-      bordered  
-    >
-      <q-item>
-        <q-item-section avatar>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-          </q-avatar>
-        </q-item-section>
+<q-page class="constrain q-pa-md">
 
-        <q-item-section>
-          <q-item-label class="text-bold">hyperYuri</q-item-label>
-          <q-item-label caption>
-            {{ post.location}}
-          </q-item-label>
-        </q-item-section>
-      </q-item>
+  <div class="row q-col-gutter-lg">
+    <div class="col-12 col-sm-8">
+      <q-card v-for="post in posts" :key="post.id" class="card-post q-mb-md" flat bordered>
+        <q-item>
+          <q-item-section avatar>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+          </q-item-section>
 
-      <q-separator />
-      
-      <q-img
-        :src="post.imageUrl"
-      />
+          <q-item-section>
+            <q-item-label class="text-bold">hyperYuri</q-item-label>
+            <q-item-label caption>
+              {{ post.location}}
+            </q-item-label>
+          </q-item-section>
+        </q-item>
 
-            <q-card-section>
-        <div>{{post.caption}}</div>
-        <div class="text-caption text-grey">
-          {{post.date | niceDate }}
-        </div>
-      </q-card-section>
+        <q-separator />
 
-    </q-card>
-  </q-page>
+        <q-img :src="post.imageUrl" />
+
+        <q-card-section>
+          <div>{{post.caption}}</div>
+          <div class="text-caption text-grey">
+            {{post.date | niceDate }}
+          </div>
+        </q-card-section>
+
+      </q-card>
+    </div>
+    <div class="col-4 large-screen-only">
+      <q-item class="fixed">
+          <q-item-section avatar>
+            <q-avatar size="48px">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label class="text-bold">hyperYuri</q-item-label>
+            <q-item-label caption>
+              Yuri Alves Miguel
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+    </div>
+  </div>
+
+</q-page>
 </template>
 
 <script>
-import { date } from 'quasar';
+import {
+  date
+} from 'quasar';
 
 export default {
   name: 'PageHome',
   data() {
     return {
-      posts: [
-        {
+      posts: [{
           id: 1,
           caption: 'Golden Gate Bridge',
           date: 1591776655504,
