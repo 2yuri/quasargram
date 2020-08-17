49f1bedb42ac854136229af51b8dc648e162a5bd
Yuri Alves Miguel <yuri.snp@hotmail.com>
2020-08-16 21:39:09 -0300

    create list of posts - caption and date

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,28 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-   <q-card class="card-post" flat bordered>
-      <q-item>
-        <q-item-section avatar>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-          </q-avatar>
-        </q-item-section>
-
-        <q-item-section>
-          <q-item-label class="text-bold">hyperYuri</q-item-label>
-          <q-item-label caption>
-            Brazil
-          </q-item-label>
-        </q-item-section>
-      </q-item>
-
-      <q-separator />
-      
-      <q-img
-        src="https://cdn.quasar.dev/img/parallax2.jpg"
-      />
-
-    </q-card>
+  <h5>camera page</h5>
   </q-page>
 </template>
 
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,6 +1,33 @@
 <template>
   <q-page class="constrain q-pa-md">
-   <h5>Home Page</h5>
+    <q-card class="card-post" flat bordered>
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-bold">hyperYuri</q-item-label>
+          <q-item-label caption>
+            Brazil
+          </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+      
+      <q-img
+        src="https://cdn.quasar.dev/img/parallax2.jpg"
+      />
+
+            <q-card-section>
+        <div >Golden gate brigde</div>
+        <div class="text-caption text-grey">june 10 9:04am</div>
+      </q-card-section>
+
+    </q-card>
   </q-page>
 </template>
 
